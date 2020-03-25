8d6fe84d917b422f9378333a1911380256cc0cb0
Your Name <you@example.com>
2020-03-25 16:13:41 +0700

    first commit

---
+++ login/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LogIn</title>
+</head>
+<body>
+    <form action="proses-login.php" method="post">
+        <table>
+            <tr>
+                <td><label for="">Username</label></td>
+                <td><input type="text" name="username" id=""></td>
+            </tr>
+            <tr>
+                <td><label for="">Password</label></td>
+                <td><input type="password" name="password" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="login" name="btnlogin"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
